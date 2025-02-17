22ba95364705193500aa1fdbbbe54fba01d2d808
Ernesto Rampanelli Neto <ghostnetto@gmail.com>
2025-02-17 18:32:37 -0300

    Ex compleat

--- exercicios/ex009/ex009.js
+++ exercicios/ex009/ex009.js
@@ -1,7 +1,17 @@
-let país = "França" 
-console.log('Vivendo em ' + país + ' Você é')
-if(país == 'Brasil' || país == 'brasil'){
-    console.log("brasileiro")
-}else{
-    console.log('estrangeiro')
+// let país = "França" 
+// console.log('Vivendo em ' + país + ' Você é')
+// if(país == 'Brasil' || país == 'brasil'){
+//     console.log("brasileiro")
+// }else{
+//     console.log('estrangeiro')
+// }
+
+function Enviar(){
+    let países = document.getElementById('países2id').value;
+    let texto2 = document.getElementById('texto')
+    if(países == "BR"){
+        texto2.innerHTML = 'Você é brasileiro'
+    }else{
+        texto2.innerHTML = 'Você é estrangeiro'
+    }
 }
