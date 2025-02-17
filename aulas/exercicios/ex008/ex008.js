721983c4025605907c27a162982eb89947e04803
Ernesto Rampanelli Neto <ghostnetto@gmail.com>
2025-02-17 15:41:52 -0300

    Ex

---
+++ aulas/exercicios/ex008/ex008.js
@@ -0,0 +1,7 @@
+let telescopio_tamanho = 91;
+
+if(telescopio_tamanho > 90){
+    console.log(`Seu telescópio de ${telescopio_tamanho}mm é muito grande para carregar na bolsa, use um menor!`)
+}else{
+    console.log(`Seu telescopio te` + telescopio_tamanho`mm você pode guarda-lo na mochila`)
+}
