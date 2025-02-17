3450b3caea6eb49fbf2a8a1bb5fe81950c0c6b56
Ernesto Rampanelli Neto <ghostnetto@gmail.com>
2025-02-17 17:11:29 -0300

    ex de condições

---
+++ exercicios/ex009/ex009.js
@@ -0,0 +1,7 @@
+let país = "França" 
+console.log('Vivendo em ' + país + ' Você é')
+if(país == 'Brasil' || país == 'brasil'){
+    console.log("brasileiro")
+}else{
+    console.log('estrangeiro')
+}
