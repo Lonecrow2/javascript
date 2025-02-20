1e79a4d3465a0dfa2d8684e4090765aad11ef6ed
Ernesto Rampanelli Neto <ghostnetto@gmail.com>
2025-02-20 14:04:52 -0300

    😮

--- exercicios/ex016_r1/script.js
+++ exercicios/ex016_r1/script.js
@@ -14,11 +14,37 @@ function verificar() {
         img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >= 0 && idade < 12) {
+                //criança
+                img.setAttribute('src', 'imagens/m_criança.png')
+            } else if (idade < 29) {
+                //jovem
+                img.setAttribute('src', 'imagens/m_jovem.png')
+            } else if (idade < 59) {
+                //adulto
+                img.setAttribute('src', 'imagens/m_adulto.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/m_velho.png')
+            }
         }
         else if (fsex[1].checked) {
             genero = 'mulher'
+                if (idade >= 0 && idade < 12) {
+                    //criança
+                    img.setAttribute('src', 'imagens/f_criança.png')
+                } else if (idade < 29) {
+                    //jovem
+                    img.setAttribute('src', 'imagens/f_jovem.png')
+                } else if (idade < 59) {
+                    //adulto
+                    img.setAttribute('src', 'imagens/f_adulto.png')
+                } else {
+                    //idoso
+                    img.setAttribute('src', 'imagens/f_velho.png')
+                }
+            }
+            res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos</p>`
+            res.appendChild(img)
         }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
     }
-
-}
